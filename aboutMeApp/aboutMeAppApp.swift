8959fc87339d22f14dba42946d89351468d06f7c
Kristen Nwafor <kcn6@rice.edu>
2023-04-23 14:07:51 -0500

    Initial Commit

---
+++ aboutMeApp/aboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeAppApp.swift
+//  aboutMeApp
+//
+//  Created by Kristen Nwafor on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
